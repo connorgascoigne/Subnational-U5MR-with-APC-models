a3041209c77c229cb8fb8ca6f0fda725fc9fca61
Gascoigne <cgascoig@ic.ac.uk>
2024-10-08 14:35:29 +0100

    file cleaning

--- Code/crossValidation.R
+++ Code/crossValidation.R
@@ -1,4 +1,5 @@
 rm(list = ls())
+
 ## ENTER COUNTRY OF INTEREST -----------------------------------------------
 # Please capitalize the first letter of the country name and replace " " in the country name to "_" if there is.
 country <- 'Kenya'
